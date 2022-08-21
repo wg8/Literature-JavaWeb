938ee22333623ddbe188bd4db5d9f000b5f9a62d
Harry <wenguang248@gmail.com>
2022-08-21 09:28:05 +0800

    create project

---
+++ src/main/java/com/wg8/literature/LiteratureApplication.java
@@ -0,0 +1,13 @@
+package com.wg8.literature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteratureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteratureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wg8/literature/LiteratureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wg8.literature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteratureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
